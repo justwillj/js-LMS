e6fc890115a6314743baf8d24d2a2f7b7212a01c
Justin Williams <williams.jman@gmail.com>
2022-10-17 21:43:27 -0400

    Data Model for a Submission with testable data

---
+++ index.js
@@ -0,0 +1,86 @@
+//Data Model for a Submission
+console.log("hello");
+const test1 = [
+  {
+    quizName: "Javascript",
+    quizModule: "Statistics",
+    quizScore: 88.2,
+    studentId: 10,
+    studentName: "Justin",
+    submissionDate: "10/16/2022",
+  },
+
+  {
+    quizName: "Javascript",
+    quizModule: "Statistics",
+    quizScore: 94.7,
+    studentId: 12,
+    studentName: "Jake",
+    submissionDate: "10/16/2022",
+  },
+
+  {
+    quizName: "Javascript",
+    quizModule: "Statistics",
+    quizScore: 70.3,
+    studentId: 14,
+    studentName: "Bob",
+    submissionDate: "10/15/2022",
+  },
+  {
+    quizName: "HTML",
+    quizModule: "CS",
+    quizScore: 73.2,
+    studentId: 10,
+    studentName: "Justin",
+    submissionDate: "10/17/2022",
+  },
+
+  {
+    quizName: "HTML",
+    quizModule: "CS",
+    quizScore: 82.2,
+    studentId: 12,
+    studentName: "Jake",
+    submissionDate: "10/18/2022",
+  },
+
+  {
+    quizName: "HTML",
+    quizModule: "CS",
+    quizScore: 66.1,
+    studentId: 14,
+    studentName: "Bob",
+    submissionDate: "10/18/2022",
+  },
+  
+];
+
+const test2 = [
+  {
+    quizName: "CSS",
+    quizModule: "Algebra",
+    quizScore: 88.1,
+    studentId: 10,
+    studentName: "Justin",
+    submissionDate: "10/16/2022",
+  },
+
+  {
+    quizName: "CSS",
+    quizModule: "Algebra",
+    quizScore: 94,
+    studentId: 12,
+    studentName: "Jake",
+    submissionDate: "10/16/2022",
+  },
+
+  {
+    quizName: "CSS",
+    quizModule: "Algebra",
+    quizScore: 70,
+    studentId: 14,
+    studentName: "Bob",
+    submissionDate: "10/16/2022",
+  },
+];
